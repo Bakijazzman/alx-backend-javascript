20cffcc62aa3dd222c80115a44ba1c590832dc4d
Bakijazzman <obafemiolabayo@gmail.com>
2024-06-05 13:40:05 +0100

    get students

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,3 @@
-
 export default function getStudentsByLocation(arrayOfObjects, city) {
   if (Array.isArray(arrayOfObjects)) {
     const iDArray = arrayOfObjects.filter((item) => item.location === city);
